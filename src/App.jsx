d900393f2e7dcbe1c3c4a1b204dd5bb47a31a87d
Rajshree Bargal <bargalrajshree@gmail.com>
2024-06-28 19:23:46 +0530

    create responsive  Medicine Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Registration from './pages/Registration'
 import Login from './pages/Login'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Medicine from './pages/medicine/Medicine'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Registration />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/medicine" element={<Medicine/>} />
         </Routes>
       </Router>
 
