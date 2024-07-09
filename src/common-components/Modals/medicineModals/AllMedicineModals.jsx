cc63e1ffe213a304e7549475c5666722ad56cc3b
Rajshree Bargal <bargalrajshree@gmail.com>
2024-07-09 15:34:46 +0530

    display popup msg after submit and close popup

--- src/common-components/Modals/medicineModals/AllMedicineModals.jsx
+++ src/common-components/Modals/medicineModals/AllMedicineModals.jsx
@@ -1,41 +1,17 @@
 import React, { useState } from "react";
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Box } from "@mui/material";
+import { Button, Dialog, DialogContent, DialogContentText, DialogTitle, Box } from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import AddCategoryModal from "./addMedicineModals/AddCategoryModal";
 import AddMedicineModal from "./addMedicineModals/AddMedicineModal";
 import AddUnitsModal from "./addMedicineModals/AddUnitsModal";
 import AddMedicineTypeModal from "./addMedicineModals/AddMedicineTypeModal";
 
-const AllMedicineModals = ({ open, handleClose, formType , style}) => {
+const AllMedicineModals = ({ open, handleClose, formType, style }) => {
   const [categoryName, setCategoryName] = useState('');
   const [medicineName, setMedicineName] = useState('');
   const [unitName, setUnitName] = useState('');
   const [medicineTypeName, setMedicineTypeName] = useState('');
   const [success, setSuccess] = useState(false);
-
-  const handleAddCategory = () => {
-    setTimeout(() => {
-      setSuccess(true);
-    }, 500);
-  };
-
-  const handleAddMedicine = () => {
-    setTimeout(() => {
-      setSuccess(true);
-    }, 500);
-  };
-
-  const handleAddUnit = () => {
-    setTimeout(() => {
-      setSuccess(true);
-    }, 500);
-  };
-
-  const handleAddMedicineType = () => {
-    setTimeout(() => {
-      setSuccess(true);
-    }, 500);
-  };
 
   const resetForm = () => {
     setCategoryName('');
@@ -59,34 +35,15 @@ const AllMedicineModals = ({ open, handleClose, formType , style}) => {
       case 'add medicinetype':
         return <AddMedicineTypeModal medicineTypeName={medicineTypeName} setMedicineTypeName={setMedicineTypeName} />;
       case 'add units':
-        return <AddUnitsModal unitName={unitName} setUnitName={setUnitName} />;
+        return <AddUnitsModal unitName={unitName} setUnitName={setUnitName} setSuccess={setSuccess} />;
       default:
         return null;
     }
   };
 
-  const handleSubmit = () => {
-    switch (formType) {
-      case 'add category':
-        handleAddCategory();
-        break;
-      case 'add medicine':
-        handleAddMedicine();
-        break;
-      case 'add medicinetype':
-        handleAddMedicineType();
-        break;
-      case 'add units':
-        handleAddUnit();
-        break;
-      default:
-        break;
-    }
-  };
-
   return (
     <Dialog open={open} onClose={handleDialogClose} PaperProps={{ style }}>
-      <DialogTitle sx={{color:'#086070',fontWeight:'600'}}>{formType.charAt(0).toUpperCase() + formType.slice(1)}</DialogTitle>
+      <DialogTitle sx={{ color: '#086070', fontWeight: '600' }}>{formType.charAt(0).toUpperCase() + formType.slice(1)}</DialogTitle>
       <DialogContent>
         {!success ? (
           renderForm()
@@ -96,16 +53,10 @@ const AllMedicineModals = ({ open, handleClose, formType , style}) => {
             <DialogContentText sx={{ mt: 2 }}>
               {formType.charAt(0).toUpperCase() + formType.slice(1)} added successfully!
             </DialogContentText>
+            <Button onClick={handleDialogClose} variant="contained" color="primary">Close</Button>
           </Box>
         )}
       </DialogContent>
-      {/* <DialogActions>
-        {!success ? (
-          <Button onClick={handleSubmit} variant="contained" color="primary">Add</Button>
-        ) : (
-          <Button onClick={handleDialogClose} variant="contained" color="primary">Close</Button>
-        )}
-      </DialogActions> */}
     </Dialog>
   );
 };
